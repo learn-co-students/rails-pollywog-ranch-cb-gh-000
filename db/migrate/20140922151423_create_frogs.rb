32a736289e69cce48bee1b146f15d27058965ec7
Aurangzaib <aurangzaib.danial@gmail.com>
2020-03-17 13:08:27 +0500

    Done.

--- db/migrate/20140922151423_create_frogs.rb
+++ db/migrate/20140922151423_create_frogs.rb
@@ -1,3 +1,7 @@
-class CreateFrogs < ActiveRecord::Migration
-  # code goes here
+class CreateFrogs < ActiveRecord::Migration[5.0]
+  create_table :frogs do |t|
+    t.string :name
+    t.string :color
+    t.integer :pond_id
+  end
 end
--- db/migrate/20140922151507_create_ponds.rb
+++ db/migrate/20140922151507_create_ponds.rb
@@ -1,3 +1,6 @@
-class CreatePonds < ActiveRecord::Migration
-  # code goes here
+class CreatePonds < ActiveRecord::Migration[5.0]
+  create_table :ponds do |t|
+    t.string :name
+    t.string :water_type
+  end
 end
